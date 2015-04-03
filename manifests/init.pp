ba8885b9eccb7a798430193a4744d2b19c4fca6c
Andreina Loriente <ninina31@gmail.com>
2015-04-03 21:12:15 +0200

    initial files

---
+++ manifests/init.pp
@@ -0,0 +1,98 @@
+class install {	
+
+	class {'apache':}
+
+	apache::vhost { 'portal.dev':
+	      port          => '80',
+	      docroot       => '/www/',
+	      docroot_owner => 'vagrant',
+	      docroot_group => 'vagrant',
+	}
+
+	apache::vhost { 'portal.pro':
+	      port          => '80',
+	      docroot       => '/www/',
+	      docroot_owner => 'vagrant',
+	      docroot_group => 'vagrant',
+	}
+
+	####################################################### php ###############################################################
+	class {'yum':}
+	
+	#PHP
+	$php_version = '56'
+	$yum_repo = 'remi-php56'
+	::yum::managed_yumrepo { 'remi-php56':
+	  descr          => 'Les RPM de remi pour Enterpise Linux $releasever - $basearch - PHP 5.6',
+	  mirrorlist     => 'http://rpms.famillecollet.com/enterprise/$releasever/php56/mirror',
+	  enabled        => 1,
+	  gpgcheck       => 1,
+	  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
+	  gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-remi',
+	  priority       => 1,
+	}
+	
+	class { 'php':
+	  version => 'latest',
+	  require => Yumrepo[$yum_repo]
+	}
+
+	####################################################### php ###############################################################
+
+	#class { 'yum':
+	#    defaultrepo => false,
+	#    extrarepo   => '' ,
+	#}
+
+	class { 'yum::repo::mysql_community':
+	    enabled_version => '5.5',
+	}
+
+	  package { 'mysql-community-server':
+	    ensure  => '5.5.42-2.el6',
+	    require => Class['yum::repo::mysql_community'],
+	}
+
+	#class {'::mysql::server':
+	#	root_password    => 'root',
+	#  	override_options => { 'mysqld' => { 'max_connections' => '512' } }
+	#}
+	#
+
+	class { 'init_mysql':
+		subscribe 	=> Class['yum::repo::mysql_community'],
+	}
+
+	mysql_database { 'portaldb':
+	  	ensure  => 'present',
+	  	charset => 'latin1',
+	  	collate => 'latin1_swedish_ci',
+	  	subscribe 	=> Class['init_mysql']
+	}
+
+	class {'memcached':}
+
+	class { '::ntp':
+		servers => [ '1.es.pool.ntp.org', '2.europe.pool.ntp.org', '3.europe.pool.ntp.org' ],
+		package_ensure => 'latest'
+	}
+
+	$misc_packages = ['vim-enhanced','telnet','zip','unzip','screen','libssh2','libssh2-devel', 'gcc', 'gcc-c++', 'autoconf', 	'automake']
+	
+	package { $misc_packages: ensure => latest }
+
+	class {'stdlib':}
+	class {'git': 
+		subscribe 	=> Class['stdlib']
+	}
+
+	class { 'composer':
+  		command_name => 'composer',
+  		target_dir   => '/usr/local/bin',
+	}
+
+	class { 'timezone':
+    	timezone => 'Europe/Madrid',
+	}
+
+}	
